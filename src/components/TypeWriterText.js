0228f0d58dbebc8f0a950307e23fb6d8d22a4667
Faisallm <faisallawan1997@gmail.com>
2024-01-13 02:09:38 -0800

    Working on Home

---
+++ src/components/TypeWriterText.js
@@ -0,0 +1,54 @@
+import React from "react";
+import styled from "styled-components";
+import Typewriter from "typewriter-effect";
+
+const Title = styled.h1`
+  font-size: ${(props) => props.theme.fontxxl};
+  text-transform: capitalize;
+  width: 80%;
+  color: ${props => props.theme.text};
+
+  span {
+    text-transform: uppercase;
+    font-family:'Akaya Telivigala', cursive;
+  }
+  .text-1 {
+    color: blue;
+  }
+  .text-2 {
+    color: orange;
+  }
+  .text-3 {
+    color: red;
+  }
+
+`;
+
+const TypeWriterText = () => {
+  return (
+    <Title>
+      Discover a new era of cool
+      <Typewriter
+      options={{
+        autoStart: true,
+        loop: true,
+      }}
+        onInit={(typewriter) => {
+          typewriter
+            .typeString('<span class="text-1">NFTs.</span>')
+            .pauseFor(2000)
+            .deleteAll()
+            .typeString('<span class="text-2">Collectible Items.</span>')
+            .pauseFor(2000)
+            .deleteAll()
+            .typeString('<span class="text-3">Ape Killers!</span>')
+            .pauseFor(2000)
+            .deleteAll()
+            .start();
+        }}
+      />
+    </Title>
+  );
+};
+
+export default TypeWriterText;
--- src/components/sections/Home.js
+++ src/components/sections/Home.js
@@ -1,8 +1,49 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import TypeWriterText from '../TypeWriterText';
+
+const Section = styled.section`
+min-height: ${props => `calc(100vh - ${props.theme.navHeight})`};
+width: 100vw;
+// positioned relative to its original position
+position: relative;
+background-color: ${props => props.theme.text};
+`
+
+const Container = styled.div`
+width: 75%;
+min-height: 80vh;
+// auto center
+margin: 0 auto;
+background-color: lightblue;
+
+display: flex;
+justify-content: center;
+align-items: center;
+`
+
+const Box = styled.div`
+width: 50%;
+height: 100%;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+// border: 1px solid black;
+`
 
 const Home = () => {
   return (
-    <div>Home</div>
+
+    <Section>
+
+      <Container>
+        <Box><TypeWriterText/></Box>
+        <Box><h1>video</h1></Box>
+      </Container>
+
+    </Section>
+
   )
 }
 
